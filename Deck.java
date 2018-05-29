e11935f6f27e0388401da76ea66774007085bc21
Mira Jacobs <mirajacobs@Mitchs-MacBook-Pro.local>
2018-05-29 14:16:15 -0400

    Added files

---
+++ Deck.java
@@ -0,0 +1,9 @@
+import java.util.*;
+//This will be the main deck for the cards
+
+public class Deck{
+
+  public Deck(){
+  
+  }
+}
---
+++ PandemicGame.java
@@ -0,0 +1,17 @@
+//packages to be imported
+import java.util.Scanner;
+import java.io*;
+import java.util.*;
+import java.io.IOException;
+
+
+//Main Game
+public class PandemicGame{
+  //this is the main
+  public static vois main(String[] args)throws IOException{
+  
+  }
+
+
+}
+//Other methods
