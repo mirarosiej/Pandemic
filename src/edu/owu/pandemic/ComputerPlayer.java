f5ef43b91a88306897c3e17bac0af6e0ef145643
Epowerj <epowerj@gmail.com>
2018-06-04 11:16:01 -0400

    Added simple ai framework

---
+++ src/edu/owu/pandemic/ComputerPlayer.java
@@ -0,0 +1,17 @@
+package edu.owu.pandemic;
+
+public class ComputerPlayer {
+    private GameState gamestate;
+    private Player player;
+
+    public ComputerPlayer(GameState gamestatelink, Player tocontrol){
+        gamestate = gamestatelink;
+        player = tocontrol;
+    }
+
+    public String doMove(){
+        //TODO do move and return string description of move
+
+        return "Did nothing";
+    }
+}
